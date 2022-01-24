9a49bc4614c4c9d0037309dff9c8333f0f1a8ff9
syedmalikali <syedmalikali@gmail.com>
2022-01-24 08:23:58 +0300

    first commit

---
+++ alhoda/alhoda/doctype/test2/test2.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Syed Malik Ali and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Test2', {
+	// refresh: function(frm) {
+
+	// }
+});
