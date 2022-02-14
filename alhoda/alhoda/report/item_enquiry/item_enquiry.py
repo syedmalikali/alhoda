227ce9bd5fcf1f7cb3874423049746669a500fd0
syedmalikali <syedmalikali@gmail.com>
2022-02-14 11:59:26 +0300

    it

--- alhoda/alhoda/report/item_enquiry/item_enquiry.py
+++ alhoda/alhoda/report/item_enquiry/item_enquiry.py
@@ -8,11 +8,13 @@ def execute(filters=None):
 def get_data(filters):
 	print(f"\n\n\n{filters}\n\n\n")
 
-	data = frappe.db.sql("""SELECT item_code,warehouse,actual_qty from `tabBin`;""")
+	data = frappe.db.sql("""SELECT item_code,warehouse,actual_qty,reserved_qty,format(valuation_rate,2)  from `tabBin`;""")
 	return data
 def get_columns():
 	return[
 		"Item Code:Link/bin:100",
-		"Warehouse:Data/warehouse:70",
+		"Warehouse:Data/warehouse:150",
 		"Current Stock:Data/actual_qty:150",
+		"Reseved Qty:reserved_qty:150",
+		"Avg.Cost:valuation_rate:150",
 	]
